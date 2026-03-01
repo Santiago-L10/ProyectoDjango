025088810f821564803ac25afb653134bcf7c043
pipejfdv <juampi_03_33@hotmail.com>
2026-03-01 12:05:28 -0500

    Modelo de asistencia creado, se agrega camo de id de la tabla

--- gestionAcademica/asistencias/models.py
+++ gestionAcademica/asistencias/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Asistencia(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    fecha = models.DateField(auto_now=True)
+    confirmacion = models.BooleanField(default=False)
+    id_inscripcion = models.ForeignKey('inscripciones.Inscripcion', on_delete=models.CASCADE)   
+
+    def __str__(self):
+        return f"ID: {self.id} - Fecha: {self.fecha} - Confirmacion: {self.confirmacion} - Inscripcion: {self.id_inscripcion}"
