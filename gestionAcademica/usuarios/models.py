e873924e94d40079f8e9a1d67cc20aa5d80233b4
Santiago.L <santiagoleon009@gmail.com>
2026-03-02 16:07:30 -0500

    Creacion de modulos Usuario (con la relacion de rol y tipo de documento) y Inscripcion, se agregan los modulos en los archivos admin y ya se subieron al sqlite

--- gestionAcademica/usuarios/models.py
+++ gestionAcademica/usuarios/models.py
@@ -1,3 +1,32 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Rol(models.Model):
+    id_rol = models.UUIDField(default = uuid.uuid4, unique=True, editable=False, 
+                              primary_key=True)
+    nombre_rol =models.CharField(max_length=20)
+
+
+class TipoDocumento(models.Model):
+    id_tipo_doc = models.UUIDField(default = uuid.uuid4, unique=True, editable=False, 
+                              primary_key=True)
+    nombre_tipo_doc =models.CharField(max_length=20)
+
+
+class Usuario(models.Model):
+    id_usuario = models.BigIntegerField(unique=True, db_index=True, primary_key=True)
+    nombre_usuario = models.CharField(max_length=200)
+    activo = models.BooleanField(default=True)
+    id_tipo_documento = models.ForeignKey(
+        TipoDocumento,
+        on_delete=models.PROTECT,
+        related_name="tiposDocumento",
+        related_query_name="tipoDocumento"
+    )
+    id_rol = models.ForeignKey(
+        Rol,
+        on_delete=models.PROTECT,
+        related_name="roles",
+        related_query_name="rol"
+    )
+    
